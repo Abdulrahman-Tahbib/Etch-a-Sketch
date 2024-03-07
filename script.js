61340d4dc61b92a4733c02fc9566a7533015cb79
Abdulrahman-tahbib <Abdulrahman.tahbib@gmail.com>
2024-03-07 19:10:55 +0300

    Add button to resize the grid in Etch a Sketch

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-
-var rows = 64;
-
+var rows = 16;
 let hi = 384 / rows;
-
 const container = document.querySelector("#container");
+var grid = document.createElement('div');            // creates grid
+grid.className = 'grid';
+
+for (var i = 0; i < rows; ++i) {
+    var column = document.createElement('div');      // creates column
+    column.className = 'column';
+    for (var j = 0; j < rows; ++j) {
+        var cell = document.createElement('div');    // creates cell
+        cell.className = 'cell';
+        cell.setAttribute('style', 'height:'+hi+'px;');
+        column.appendChild(cell);                    // appends cell to column (as row)
+    }
+    grid.appendChild(column);                        // appends column to grid
+}
+
+container.appendChild(grid);                     // appends grid to webpage
+
+const cel = document.querySelectorAll('.cell');
+cel.forEach((div) => {
+    div.addEventListener('mouseenter' , () => {
+        div.setAttribute('style', 'background: black; outline: solid  black; height:'+hi+'px;');
+      });
+});
+
 
 const btn = document.querySelector('button');
 
-btn.addEventListener('click' , () => {rows = prompt("Enter new grid size")});
-
-
-
-
-
-var grid = document.createElement('div');            // creates grid
+btn.addEventListener('click' , () => {
+    const element = document.querySelector('.grid');
+    element.remove();
+    rows = prompt("Enter new grid size");
+    let hi = 384 / rows;
+    var grid = document.createElement('div');            // creates grid
 grid.className = 'grid';
 for (var i = 0; i < rows; ++i) {
     var column = document.createElement('div');      // creates column
@@ -27,13 +47,10 @@ for (var i = 0; i < rows; ++i) {
     grid.appendChild(column);                        // appends column to grid
 }
 container.appendChild(grid);                     // appends grid to webpage
-
-
 const cel = document.querySelectorAll('.cell');
-
 cel.forEach((div) => {
     div.addEventListener('mouseenter' , () => {
         div.setAttribute('style', 'background: black; outline: solid  black; height:'+hi+'px;');
       });
 });
-
+});
